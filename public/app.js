5e924dfc98720dab4b47c8c53a8bf8e9435edfc6
Samah <samahalimuhammad@gmail.com>
2021-07-05 10:28:41 +0200

    remove feker package

--- public/app.js
+++ public/app.js
@@ -1,4 +1,6 @@
 const auth = firebase.auth();
+const { serverTimestamp } = firebase.firestore.FieldValue;
+
 
 // Grap HTML elements
 const whenSignedIn = document.getElementById("whenSignedIn")
@@ -12,19 +14,51 @@ const userDetails = document.getElementById("userDetails")
 //login with google account
 const provider = new firebase.auth.GoogleAuthProvider();
 
-signInBtn.onclick = ()=> auth.signInWithPopup(provider);
-signOutBtn.onclick = ()=> auth.signOut();
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+signOutBtn.onclick = () => auth.signOut();
 
 auth.onAuthStateChanged(user => {
-    if(user){
+    if (user) {
         //Sign in
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
         userDetails.innerHTML = `<h3> Hello ${user.displayName}! </h3> <p>User ID: ${user.uid}</p>`
-    }else{
+    } else {
         //not Sign in
         whenSignedIn.hidden = true;
         whenSignedOut.hidden = false;
         userDetails.innerHTML = '';
     }
 })
+
+// Firestore
+
+
+const db = firebase.firestore();
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+
+let thingsRef;      //reference to database location
+//let unsubscribe;      // turn off realtime streaming
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //Sign in
+        thingsRef = db.collection('things')
+
+        createThing.onclick = () => {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
+                uid: user.uid,
+                name: "",
+                createdAt: serverTimestamp()
+            });
+        }
+
+
+    }
+});
